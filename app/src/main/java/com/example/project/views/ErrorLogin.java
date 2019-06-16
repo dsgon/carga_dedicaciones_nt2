f5227eb548f90f004792e08a75f0f771bab701bd
David Santiago <davidsghz@gmail.com>
2019-06-16 01:48:18 -0300

    Se valida proceso de login contra XML de colaboradores, si es exitoso te envia a la view Dedicaciones, sino a la de Error Login.

--- app/src/main/java/com/example/project/views/ErrorLogin.java
+++ app/src/main/java/com/example/project/views/ErrorLogin.java
@@ -1,15 +1,25 @@
 package com.example.project.views;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
 
-import com.example.project.R;
-
-public class ErrorLogin extends AppCompatActivity {
+public class ErrorLogin extends AppCompatDialogFragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_error_login);
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder
+                .setTitle("Login Error")
+                .setMessage("El usuario o el password son incorrectos. Verifique")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
     }
 }
