1eb8cb7649923a69c8e467b04f408782a4e5f5ab
David Santiago <davidsghz@gmail.com>
2019-07-08 12:50:24 -0300

    Add FileHandle para manejo de la extension de los archivos

---
+++ app/src/main/java/com/example/project/helper/FileHandle.java
@@ -0,0 +1,9 @@
+package com.example.project.helper;
+
+public class FileHandle {
+
+    private static final String FILE_EXTENSION = ".txt";
+
+    public static String getFileExtension(){return  FILE_EXTENSION; }
+
+}
